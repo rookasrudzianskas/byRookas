6741bd3ef35a67d7a80d301a8dc84bca65d764cc
Rokas <rookas.rudzenskas@gmail.com>
2021-09-21 19:08:37 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,19 +4,19 @@ export default function Home() {
 
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-black">
       <Head>
         <title>byRookas</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1>Hello, Let's build the awesome shit</h1>
+        <h1 className="text-white text-7xl font-bold">Hello, Let's build the awesome shit 🚀</h1>
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
-          className="flex items-center justify-center"
+          className="flex items-center justify-center text-white"
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
