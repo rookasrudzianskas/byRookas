07a9fc82e97ae1337d9d8e1c744655fdaa2cfca7
Rokas <rookas.rudzenskas@gmail.com>
2021-09-21 19:33:32 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,12 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+      {/*      her  */}
+          <div className="">
+          {/*    taglines   */}
+          {/*    video  */}
 
+          </div>
       </main>
 
       <footer className="flex items-center justify-center w-full h-24 border-t border-gray-900">
