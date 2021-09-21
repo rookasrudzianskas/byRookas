e02e69f0ffc512b03d6aaee154aa1590d00f085a
Rokas <rookas.rudzenskas@gmail.com>
2021-09-21 18:47:57 +0300

    Rokas

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }) {
+
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-
 export default MyApp
