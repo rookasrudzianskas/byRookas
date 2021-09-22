d72b1ec32d6f92c1c945c5e80919e3de418311d9
Rokas <rookas.rudzenskas@gmail.com>
2021-09-22 13:49:44 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,7 @@ export default function Home() {
     ];
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-900">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-900 scrollbar-hide">
       <Head>
         <title>byRookas</title>
         <link rel="icon" href="/favicon.ico" />
