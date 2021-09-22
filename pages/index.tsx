1e494c3bda200c5ec83c76e99e35fc00d87e4556
Rokas <rookas.rudzenskas@gmail.com>
2021-09-22 13:39:44 +0300

    Rokas

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home() {
           {/*    taglines   */}
               <div className="flex flex-col justify-center items-center px-3 md:flex-1">
                   <span className="text-blue-300 text-md md:text-left">Development | Code | Entrepreneurship</span>
-                  <h1 className="text-gray-100 text-3xl font-bold  mt-4 md:text-left">Your journey to becoming an awesome developer!</h1>
+                  <h1 className="text-gray-100 text-3xl font-bold  mt-4 md:text-left">Rokas Rudzianskas, creative entrepreneur and creative cloud junky.</h1>
 
                   <p className="text-gray-300 mt-6 md:text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci earum provident quisquam voluptatum. Architecto assumenda laboriosam perferendis sit suscipit voluptas?</p>
                   <div className="bg-yellow-300 px-4 py-2 rounded-md mt-5 hover:bg-yellow-600 transition duration-150 ease-in-out">
@@ -38,17 +38,6 @@ export default function Home() {
                   </div>
               </div>
 
-          {/*    video  */}
-          {/*    <div className="flex items-center justify-center mt-5 px-10 mr-6 md:flex-1 px-3">*/}
-          {/*        <div className="bg-red-500 flex-1 z-0 relative">*/}
-          {/*          <Image height={144} width={144} layout="responsive" className="" src={'https://images.unsplash.com/photo-1632224120647-e7abd8225fb4?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'} />*/}
-
-          {/*            <div className="absolute top-0 right-0 w-14 transform translate-x-9 translate-y-1 md:absolute right-0 top-64 transform translate-x-9 translate-y-64 lg:absolute right-0 transform translate-y-60">*/}
-          {/*                <Image height={2106} width={1204} layout="responsive" className="rounded-md" src="/pattern_4.svg" />*/}
-          {/*            </div>*/}
-          {/*        </div>*/}
-
-          {/*    </div>*/}
               <div className="flex-1 relative justify-center items-center pr-9 mb-10 w-full px-5 mt-6">
                   <Image
                       src={'https://images.unsplash.com/photo-1632224120647-e7abd8225fb4?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'}
