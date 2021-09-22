5d93a8e543594949048cc4297e77e0a14c9261b2
Rokas <rookas.rudzenskas@gmail.com>
2021-09-22 14:18:36 +0300

    Rokas

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Image from "next/image";
+import Button from "./Button";
 
 interface BlogCardProps {
     post: {
         id: string,
         slug: string,
         title: string,
-        except: string,
+        excerpt: string,
         image: string,
     }
 
@@ -14,7 +16,10 @@ interface BlogCardProps {
 const BlogCard = ({post}: BlogCardProps) => {
     return (
         <div className="">
-
+            <Image  src={post.image} layout="fill" objectFit="cover" />
+            <h1>{post?.title}</h1>
+            <p>{post?.excerpt}</p>
+            <Button text="read-more" href={`/post/${post?.slug}`} type={"tertiary"} />
         </div>
     );
 };
